17724354e86e45d8611d9488383851e7dd0c7b0a
Andrew Birt <abirt1986@gmail.com>
2021-10-15 19:38:41 -0400

    sorted harvest alphabetically, added id to harvested food, and now crop shows icon with its quantity

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,5 +1,37 @@
 export const Catalog = (foodObjArr) => {
-  foodObjArr.forEach( foodObj => {
-    document.querySelector(".container").innerHTML += `<section class="plant">${foodObj.type}</section>`;
+  
+  // Ordering the Harvest
+  // --------------------------------------------------------------------------
+  // Your first optional challenge is to display the list of food to be sold in 
+  // alphabetical order. That is, all corn should be listed first, then all potatoes, 
+  // then soybean, sunflower and wheat.
+  
+  const sortedFood = foodObjArr.sort((a, b) => a.type.localeCompare(b.type));
+
+  // Display Once With Quantity
+  // --------------------------------------------------------------------------
+  // Instead of displaying all object individually, show only one HTML 
+  // representation of each plant type and display the quantity that you are 
+  // selling of that type.
+
+  const foodWithQuantity = {};
+
+  sortedFood.forEach(food => {
+    if (!foodWithQuantity[food.type]) {
+      foodWithQuantity[food.type] = { quantity: 1, icon: food.icon };
+    } else {
+      foodWithQuantity[food.type].quantity++;
+    }
   });
+
+  // console.log(foodWithQuantity)
+
+  // sortedFood.forEach( foodObj => {
+  //   document.querySelector(".container").innerHTML += `<section class="plant">${foodObj.icon}</section>`;
+  // });
+
+  for (const food in foodWithQuantity) {
+    document.querySelector(".container").innerHTML += `<section class="plant">${foodWithQuantity[food].icon}: ${foodWithQuantity[food].quantity}</section>`;
+  }
+
 }
--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,16 +1,26 @@
+// Unique Identifiers
+// --------------------------------------------------------------
+// Add a unique, incrementing id property to each harvested plant. 
+// The first plant harvested should have an id value of 1. 
+// The second one should have a value of 2, etc.
+
+let id = 0;
+
 export const harvestPlants = (plants) => {
   const harvested = [];
 
   plants.forEach( plant => {
+    
 
     // Make sure the cattle ranchers get thier
 
     const output = plant.type === 'Corn' ? plant.output / 2 : plant.output;
     
     for (let i = 0; i < output; i++) {
-      harvested.push(plant);
+      // Increment id before assigning to harvested plant
+      id++;
+      harvested.push({ ...plant, id: id });
     }
   });
-
   return harvested;
 }
